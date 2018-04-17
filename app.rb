ada5d83e762a1f3f686845332445809f1aa326c7
kiyoyabi <kiyora@kiyoyabi.local>
2018-04-17 11:45:46 +0900

    Aaded All Files

---
+++ app.rb
@@ -0,0 +1,25 @@
+require '/usr/local/lib/ruby/gems/2.5.0/gems/sinatra-2.0.1/lib/sinatra.rb'
+require 'sinatra/reloader'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+    adapter: 'sqlite3',
+    database: './bbs.db'
+)
+
+class Comment < ActiveRecord::Base
+  validates :body, presence: true
+end
+
+get '/' do
+  @title = "My BBS"
+  @comments = Comment.all
+  erb :index
+end
+
+post '/create' do
+  Comment.create(body: params[:body])
+  redirect to('/')
+end
+
+# <script>alert(1);</script>
